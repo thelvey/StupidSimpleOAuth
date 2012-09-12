d9ab0da5b251b53f62ee3738898674225cedf02f
thelvey <ashelvey@gmail.com>
2012-09-11 23:36:04 -0700

    Removing necessity of specific implementation classes

--- IntegrationTestHarness/Twitter/Default.aspx.cs
+++ IntegrationTestHarness/Twitter/Default.aspx.cs
@@ -5,11 +5,20 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using SimpleOAuth.OAuth;
-using SimpleOAuth.Implementations;
 
 public partial class Twitter_Default : System.Web.UI.Page
 {
     protected enum OAuthProviders { Twitter, Yelp };
+
+    protected class TwitterConfig
+    {
+        public const string RequestTokenUrl = "https://api.twitter.com/oauth/request_token";
+        public const string UserAuthUrl = "https://api.twitter.com/oauth/authorize";
+        public const string AccessTokenUrl = "https://api.twitter.com/oauth/access_token";
+        public const string MethodUrl = "https://api.twitter.com/1";
+
+        public const string TimelineUrl = MethodUrl + "/statuses/user_timeline.json";
+    }
 
     protected OAuthProviders PageProvider
     {
@@ -45,8 +54,13 @@ public partial class Twitter_Default : System.Web.UI.Page
     {
         if (String.IsNullOrEmpty(txtConsumerKey.Text)) return;
         if (String.IsNullOrEmpty(txtConsumerSecret.Text)) return;
+        
+        OAuthConsumerConfig config = new OAuthConsumerConfig();
+        config.ConsumerKey = txtConsumerKey.Text;
+        config.ConsumerSecret = txtConsumerSecret.Text;
 
-        AuthRequestResult authRequest = Twitter.GenerateUnauthorizedRequestToken(txtConsumerKey.Text, txtConsumerSecret.Text);
+
+        AuthRequestResult authRequest = OAuthClient.GenerateUnauthorizedRequestToken(config, TwitterConfig.RequestTokenUrl, TwitterConfig.UserAuthUrl);
 
         SetSessionValue("OAuthTokenSecret", authRequest.OAuthTokenSecret);
         SetSessionValue("ConsumerKey", txtConsumerKey.Text);
@@ -62,7 +76,7 @@ public partial class Twitter_Default : System.Web.UI.Page
         config.ConsumerKey = txtConsumerKey.Text;
         config.ConsumerSecret = txtConsumerSecret.Text;
 
-        AccessToken accessToken = Twitter.ExchangeForAccessToken(config, ltlOAuthToken.Text, ltlTokenSecretCallback.Text, ltlOAuthVerifier.Text);
+        AccessToken accessToken = OAuthClient.ExchangeForAccessToken(config, TwitterConfig.AccessTokenUrl, ltlOAuthToken.Text, ltlTokenSecretCallback.Text, ltlOAuthVerifier.Text);
 
         pnlAccessToken.Visible = true;
         ltlAccessToken.Text = accessToken.OAuthToken;
@@ -74,7 +88,7 @@ public partial class Twitter_Default : System.Web.UI.Page
         config.ConsumerKey = txtConsumerKey.Text;
         config.ConsumerSecret = txtConsumerSecret.Text;
 
-        dynamic tweets = Twitter.GetTweets(config, ltlAccessToken.Text, ltlAccessTokenSecret.Text);
+        dynamic tweets = OAuthClient.JsonMethod(config, TwitterConfig.TimelineUrl, ltlAccessToken.Text, ltlAccessTokenSecret.Text);
 
         grdTweets.DataSource = tweets as object[];
         grdTweets.DataBind();
